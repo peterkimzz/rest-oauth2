77bd45685bde4304f0604a424ec05a5caff34455
peterkimzz <peterkimzz69@gmail.com>
2020-07-09 15:22:48 +0900

    remove comments

--- src/base.ts
+++ src/base.ts
@@ -16,31 +16,6 @@ export abstract class BaseOAuth2 {
   }
 
   GenerateUrl() {
-    // let baseUrl;
-    // let url;
-    // switch (this.provider) {
-    //   case "google":
-    //     baseUrl = "https://accounts.google.com/o/oauth2/v2/auth";
-    //     const response_type = "code";
-    //     const scope = [
-    //       "openid",
-    //       "https://www.googleapis.com/auth/userinfo.email",
-    //       "https://www.googleapis.com/auth/userinfo.profile",
-    //     ].join(" ");
-    //     url =
-    //       baseUrl +
-    //       `?scope=${scope}&response_type=${response_type}&client_id=${this.client_id}&redirect_uri=${this.redirect_uri}`;
-    //     break;
-    //   case "facebook":
-    //     baseUrl = "https://www.facebook.com/v7.0/dialog/oauth";
-    //     break;
-    //   case "paypal":
-    //     baseUrl = this.production
-    //       ? "https://api.paypal.com"
-    //       : "https://api.sandbox.paypal.com";
-    //     break;
-    // }
-    // return url;
     throw new Error("implement required.");
   }
 
